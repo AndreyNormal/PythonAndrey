bc025e52257d6a4e48d97f51fc5bd76f41816393
AndreyNormal <69591303+AndreyNormal@users.noreply.github.com>
2024-01-26 13:51:37 +0600

    Add files via upload

---
+++ Numbers,Strings.py
@@ -0,0 +1,38 @@
+# x = 5
+# x = float(x)
+
+# x = 5.5
+# x = int(x)
+
+# x = 5
+# x = complex(x)
+
+
+# x = "Hello World"
+# print(len(x))
+
+# txt ="Hello World"
+# x = txt[0]
+
+# txt = "Hello World"
+# x = txt[2:5]
+
+# txt = "Hello World"
+# x = txt.strip()
+
+# txt ="Hello World"
+# txt = txt.upper()
+# print(txt)
+
+# txt ="Hello World"
+# txt = txt.lower()
+# print(txt)
+
+# txt = "Hello World"
+# txt = txt.replace("H","J")
+# print(txt)
+
+# age = 36
+# txt = "My name is John, and I am {}"
+# print(txt.format(age))
+# print(age)
---
+++ Syntax,comments.py
@@ -0,0 +1,12 @@
+#print("Hello world")
+
+#if (5 > 2):
+ #print("YES")
+
+# This is comment
+
+# """
+# This is a comment
+# written in 
+# more than just one line
+# """
